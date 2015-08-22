b673749879d1230ca78f6ba4a83f1c94217fc348
Vincent Yang <vincent.yang@orchard.com.au>
2015-08-22 23:56:15 +1000

    connect to db instead use hard coded array

--- public/javascripts/ng-mongo/app.js
+++ public/javascripts/ng-mongo/app.js
@@ -1,25 +1,9 @@
-var MyCtrl = function ($scope) {
-
-    $scope.items = [
-        {name: "Idiot IPA", price: 8.99, lastDrank: "2015-10-11"},
-        {name: "Inversion IPA", price: 10.99, lastDrank: "2014-05-11"},
-    ]
-
-    $scope.pluralizer = {
-        0: "No beers!",
-        1: "Only one left!",
-        other: "{} Beers in the fridge",
-    }
-
-    $scope.addBeer = function () {
-        var newBeer = {name: $scope.name, price: $scope.price};
-        $scope.items.push(newBeer);
-    }
-
-    $scope.removeItem = function (item) {
-        if(confirm("Remove this beer - it's a good one!")){
-            $scope.items.splice($scope.items.indexOf(item),1);
-        }
-    }
+var ListCtrl = function ($scope, $http) {
+    // http get returns a promise ...
+    var result = $http.get("/mongo-api/dbs");
+    
+    result.success(function (data) {
+        $scope.items = data;
+    })
 }
 
